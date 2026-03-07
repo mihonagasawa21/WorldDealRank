52e8641b634af9fb7979713ff2c77c463a100ec3
mihonagasawa21 <miho20040721@gmail.com>
2026-03-07 23:39:51 +0900

    run refresh on dbseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
-CostIndex::RefreshAll.run
+CostIndex::RefreshAll.call
