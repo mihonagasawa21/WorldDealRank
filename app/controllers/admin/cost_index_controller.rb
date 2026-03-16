b6959772efce61ae5950358753c230a91a955ad7
mihonagasawa21 <miho20040721@gmail.com>
2026-03-16 16:04:48 +0900

    delete tourist_coef && check

--- app/controllers/admin/cost_index_controller.rb
+++ app/controllers/admin/cost_index_controller.rb
@@ -1,34 +1,51 @@
-module Admin
-  class CostIndexController < BaseController
-    def refresh
-      before_final = Country.where.not(final_index: nil).count
-      before_calc  = Country.where.not(calculated_at: nil).count
+# frozen_string_literal: true
 
-      Rails.logger.info "[ADMIN COST] refresh start"
-      Rails.logger.info "[ADMIN COST] before final_index=#{before_final} calculated_at=#{before_calc}"
+class Admin::CostIndexController < ApplicationController
+  def index
+    load_cost_index_debug_data
+  end
 
-      run_rake_task!("cost_index:refresh_all")
+  def refresh
+    run_rake_task!("cost_index:refresh_all")
+    load_cost_index_debug_data
 
-      after_final = Country.where.not(final_index: nil).count
-      after_calc  = Country.where.not(calculated_at: nil).count
+    flash.now[:notice] = "本番データ更新を実行しました"
+    render :index
+  rescue => e
+    load_cost_index_debug_data rescue nil
+    flash.now[:alert] = "更新中にエラーが発生しました: #{e.class} #{e.message}"
+    render :index, status: :unprocessable_entity
+  end
 
-      Rails.logger.info "[ADMIN COST] after final_index=#{after_final} calculated_at=#{after_calc}"
+  private
 
-      load_cost_index_debug_data
+  def run_rake_task!(task_name)
+    require "rake"
+    Rails.application.load_tasks if Rake::Task.tasks.empty?
+    task = Rake::Task[task_name]
+    task.reenable
+    task.invoke
+  end
 
-      if after_final > before_final || after_calc > before_calc
-        flash.now[:notice] = "本番データ更新を実行しました"
-      else
-        flash.now[:alert] = "更新処理は走りましたが、件数は増えていません"
-      end
+  def load_cost_index_debug_data
+    countries = Country.order(:name_ja)
 
-      render :index
-    rescue => e
-      Rails.logger.error "[ADMIN COST] refresh error #{e.class}: #{e.message}"
-      Rails.logger.error e.backtrace.first(20).join("\n")
-      load_cost_index_debug_data rescue nil
-      flash.now[:alert] = "更新中にエラーが発生しました: #{e.class} #{e.message}"
-      render :index, status: :unprocessable_entity
+    @total_count = countries.count
+    @final_index_count = countries.where.not(final_index: nil).count
+    @resident_count_count = countries.where.not(jp_resident_count: nil).count
+    @calculated_at_count = countries.where.not(calculated_at: nil).count
+    @last_error_count = countries.where.not(last_error: [nil, ""]).count
+    @latest_calculated_at = countries.where.not(calculated_at: nil).maximum(:calculated_at)
+
+    @fx_count = countries.where.not(fx_rate_usd: nil).count
+    @ppp_count = countries.where.not(ppp_lcu_per_intl: nil).count
+    @plr_count = countries.where.not(plr: nil).count
+
+    @problem_countries = countries.select do |c|
+      c.final_index.nil? || c.jp_resident_count.nil? || c.last_error.present?
     end
+
+    @sample_world_rows = countries.limit(30)
+    @score_map = CostIndex::RankingScorer.new(countries).score_map
   end
 end
